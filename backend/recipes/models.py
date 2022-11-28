d2b401a79d0882093b456a4bc3388b268ba1e9a7
Anton Ignatov <a.ignatov2@yandex.ru>
2022-11-28 19:40:46 +0300

    review points 13

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -151,7 +151,7 @@ class Favorite(models.Model):
         on_delete=models.CASCADE,
     )
     recipe = models.ForeignKey(Recipes,
-                               related_name='favorites',
+                               related_name='favorite',
                                related_query_name='favorite',
                                verbose_name='Recipe',
                                on_delete=models.CASCADE)
