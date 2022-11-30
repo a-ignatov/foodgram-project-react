c21ccaeee3d1d7e223152996a17d38f7a44f4eab
Anton Ignatov <a.ignatov2@yandex.ru>
2022-11-30 19:16:40 +0300

    subs

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -138,6 +138,9 @@ class SubShowSerializer(UserShowSerializer):
             'recipes',
         )
 
+    def get_is_subscribed(self, username):
+        return True
+
     def get_recipes(self, data):
         limit = self.context.get('request').query_params.get('recipes_limit')
         if not limit:
