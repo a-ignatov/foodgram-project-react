746dc07f275d93c10ec11f71307991d81f899594
Anton Ignatov <a.ignatov2@yandex.ru>
2022-12-01 19:06:05 +0300

    count

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -132,7 +132,7 @@ class SubShowSerializer(UserShowSerializer):
     last_name = serializers.ReadOnlyField(source='following.last_name')
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
-    recipes_count = serializers.SerializerMethodField()
+    # recipes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -144,7 +144,7 @@ class SubShowSerializer(UserShowSerializer):
             'last_name',
             'is_subscribed',
             'recipes',
-            'recipes_count',
+            # 'recipes_count',
         )
 
     def get_is_subscribed(self, username):
